3cf1c7a133c1c96e236a74529481fcbbf27488af
Lennart Lewis <lenlew22@gmail.com>
2024-03-27 00:20:05 -0400

    Improve CLI argument processing and comments

---
+++ libraries/generate.py
@@ -0,0 +1,12 @@
+import random #Random Module 
+
+cards = ["jack", "queen", "king"]
+random.shuffle(cards)
+
+for card in cards: 
+    print(card)
+
+
+'''#randint method
+number = random.randint(1, 10)
+print(number)'''
