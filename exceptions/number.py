5413bed9429721d3f3363a1524a2e7c9264141fc
Lennart Lewis <lenlew22@gmail.com>
2024-03-26 23:12:02 -0400

    Completed: Exceptions

--- exceptions/number.py
+++ exceptions/number.py
@@ -1,7 +1,15 @@
-# Try and except handling value error
-try:
-    x = int(input("What's x ? "))
-except: 
-    print("x is not an integer")
-else:
+def main():
+    # Get an integer from the user and print it.
+    x = get_int("What's x? ")
     print(f"x is {x}")
+
+def get_int(prompt):
+    while True:
+        try:
+            # Return the user's input as an integer.
+            return int(input(prompt))
+        except ValueError:
+            # If input isn't an integer, retry.
+            pass
+
+main()
